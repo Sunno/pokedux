e200ea75d6024f7e1a2df5e9f573ac30aa815da1
Alvaro Marquina <amarquina@securitycompass.com>
2022-04-13 15:29:54 -0400

    10 - middlewares

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import pokemonReducer from './reducers/pokemonReducer';
 import './index.css';
+import { logActions } from './middlewares';
 
+const composerEnhancers = compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(logActions)
+    )
 const store = createStore(
     pokemonReducer, {},
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composerEnhancers
 );
 
 ReactDOM.render(
